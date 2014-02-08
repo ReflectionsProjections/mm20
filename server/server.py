5ded33fc5bc7575e43fd2c7ef2a9fc2d1e6a32fa
Nick <nickljeffrey@gmail.com>
2014-02-08 15:12:16 -0600

    created server class, needs to be cleaned up

--- server/server.py
+++ server/server.py
@@ -8,3 +8,55 @@
 #   If a connection is dropped, that player automatically 'forfeits'
 #   When the game ends, the server should send a final status to all players and then shut down gracefully.
 import SocketServer
+import sys
+import json
+from threading import Timer
+
+turnObjects = None
+validTurns = 0
+maxPlayers = 0
+currPlayers = 0
+
+class MMRunServer():
+    def __init__(self, numPlayers):
+        global maxPlayers
+        maxPlayers = numPlayers
+        turnObjects = [None for i in range(0, numPlayers)]
+        server = MMServer(("localhost", 8080), TCPHandler)
+        # terminate with Ctrl-C
+        timer = Timer(1, self.timeup)
+        timer.start()
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            sys.exit(0)
+        timer = Timer(1, self.timeup)
+        timer.start()
+    def timeup(self):
+        print "TIME'S UP"
+
+
+
+class TCPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        global currPlayers
+        if(maxPlayers == currPlayers):
+            return
+        currPlayers+=1
+        while 1:
+            data = self.request.recv(1024)
+            if not data:
+                break
+            print data
+
+class MMServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    # Ctrl-C will cleanly kill all spawned threads
+    daemon_threads = True
+    # much faster rebinding
+    allow_reuse_address = True
+    connectedClients = 0
+    def __init__(self, server_address, RequestHandlerClass):
+        SocketServer.TCPServer.__init__(self, server_address, RequestHandlerClass)
+
+if __name__ == "__main__":
+    runner = MMRunServer(1)
