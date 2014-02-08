2dd7d94bffb281ade10259827dcca57d007cd63c
Brendan Moriarty <bpmoria2@illinois.edu>
2014-02-08 14:26:27 -0600

    removed doors, added __main__

--- src/objects/Room.py
+++ src/objects/Room.py
@@ -4,17 +4,15 @@ class Room(object):
     ##
     # Passes in objects to be placed within the room.
     ##
-    def __init__(self, furniture={}, doors={}):
+    def __init__(self, furniture={}):
     """
     Initializes a room object with the following attributes
     Keyword arguments:
     furniture -- a dictionary of furniture objects to be placed in the room
                with a tuple key of coordinates for where it should be placed
-    doors -- a dictionary of door objects with a tuple for placement coordinates
     """
 
         self.furniture = furniture  # key is a tuple coordinate(?), value is object to be placed in room.
-        self.doors = doors
 
     def spawnObjects(self):
         """
@@ -52,6 +50,8 @@ class Room(object):
 
         pass
 
-
+if __name__ == "__main__":
+    room1104 = Room({(0,0),object})
+    print room1104.furniture
 
 
