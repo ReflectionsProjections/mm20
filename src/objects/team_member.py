c3dbd63ae166155233cde21ff390a8f6eb7534ae
bladechapman <your_email@example.com>
2014-02-15 13:10:36 -0600

    added attributes

--- src/objects/team_member.py
+++ src/objects/team_member.py
@@ -1,10 +1,12 @@
 class TeamMember(object):
 
-    def __init__(self,  energy):
-        self.energy = energy
-        
+    def __init__(self, name, archetype):
+        self.energy = 100
+        self.name = name
+        self.archetype = archetype
+
+
 #this is how you can do simple tests
 if __name__ == "__main__":
     sam = TeamMember(70)
     print sam.energy
-    
