dcf906ebee1797236dd644ce9a88e1721fcc4e32
Fabian Junge <fabsjunge@gmail.com>
2014-02-22 12:48:42 -0600

    started work on actions

---
+++ src/objects/action.py
@@ -0,0 +1,16 @@
+from config.handle_constants import retrieveConstants
+from exception import KeyError
+
+
+class Action:
+    actions = {}
+
+    def __init__(self, action, *args, **kwargs):
+        if not action in Action.actions:
+            raise KeyError("Action not defined")
+        self.action = action
+        self.args = args
+        self.kwargs = kwargs
+        self.key = self.kwargs.target
+
+Action.actions = retrieveConstants("actions")
