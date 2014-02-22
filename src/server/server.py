7729995161319158d44a1ec84514a5a07c81812a
Nick <nickljeffrey@gmail.com>
2014-02-22 13:55:29 -0600

    updating timer after some data received

--- src/server/server.py
+++ src/server/server.py
@@ -12,6 +12,7 @@ import json
 import select
 import socket
 from threading import Timer, Lock
+import time
 
 timeLimit = 30
 maxDataSize = 1024
@@ -24,22 +25,51 @@ class MMServer():
         #create an INET, STREAMing socket
         serversocket = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM)
-        #bind the socket to a public host,
-        # and a well-known port
-        serversocket.bind(('localhost', 8080))
+        #Set reuse address so that we don't have to wait before running again
+        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #bind the socket localhost port 8080
+        serversocket.bind(('localhost', 8088))
         #become a server socket
         serversocket.listen(5)
         playerConnections = [None for i in range(0, self.maxPlayers)]
         turnObjects = [None for i in range(0, self.maxPlayers)]
+        validTurns = 0
+        #Accept connections from correct tnumber of players
         for i in range(0, self.maxPlayers):
             (clientsocket, address) = serversocket.accept()
             playerConnections[i] = clientsocket
+        lookupPlayer = dict(zip(playerConnections, [i for i in range(0, self.maxPlayers)]))
+        currTime = time.time()
+        endTime = time.time() + timeLimit
         while 1:
-            ready = select.select(playerConnections, [], [], timeLimit)
-            data = '{}'
+            #Receive info
+            ready = None
+            if endTime - currTime > 0:
+                ready = select.select(playerConnections, [], [], endTime - currTime)
+            if ready is None:
+                #Timeout
+                for i in range(0, self.maxPlayers):
+                    if turnObjects[i] is None:
+                        turnObjects[i] = '{}'
+                        validTurns = validTurns + 1
             for connection in ready[0]:
+                #Receive data
                 data = connection.recv(maxDataSize)
-            print data
+                player = lookupPlayer[connection]
+                if turnObjects[player] is None:
+                    turnObjects[player] = data
+                    validTurns = validTurns+1
+            if validTurns == self.maxPlayers:
+                #TODO: send data to engine
+                #clear turn objects
+                validTurns = 0
+                for i in range(0, self.maxPlayers):
+                    turnObjects[i]=None
+                #reset endtime
+                currTime = time.time()
+                endTime = time.time() + timeLimit
+            else:
+                currTime = time.time()
 
 if __name__ == "__main__":
     serv = MMServer(2)
