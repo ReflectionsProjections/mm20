c82f50250d369138315af66d8a4f3cbf7acfd08c
Fabian Junge <fabsjunge@gmail.com>
2014-04-05 15:17:11 -0500

    stubbed out room and added a lot of test cases

--- src/objects/room.py
+++ src/objects/room.py
@@ -1,79 +1,105 @@
-class Room(object): #TODO: This class doesn't need to know what the furniture's location is, it just needs to know that it exists
+from unittest import TestCase, main
 
-    ## Initializes a Room object with the following attributes
-    # @param furniture A dict of furniture objects to be placed in the room of {key:value} format {name:(posX,posY)}
-    def __init__(self, furniture=dict(), color=(0, 0, 0, 0), connectedRooms=[]):
-        # Initialize parameters
-        self.furniture = furniture
-        self.name = name
+
+## This class manages "rooms" which are nodes on our locations graph.
+#  Hallways are also "rooms" in this sense.
+#  Rooms contain team members and furniture (TODO)
+class Room(object):
+
+    ## Initializes a Room object.
+    # @param name
+    #   The identifier of the room.
+    # @param furniture
+    #   Test
+    def __init__(self, name, connectedRooms=dict()):
         self.connectedRooms = connectedRooms
+        self.name = name
 
     def __str__(self):
-        return "<id:{}, connected_rooms:{}>".format(name, connectedRooms.keys)
-        
+        return "<id:{}, connected_rooms:{}>".format(self.name, self.connectedRooms.keys())
 
-    ## Get connected rooms
+    ## Returns connected rooms
     def getConnectedRooms(self):
-        return self.connected_rooms
+        return self.connectedRooms.keys()
 
-    ## Connect one room to another (both ways)
-    # @throws ValueError Thrown if self and room are already connected
-    def addConnectedRoom(self, room):
+    ## Connects one room to another
+    # @param room
+    #   The room to connect to this room
+    # @throws ValueError
+    #   Throws a value error if this room and the passed in
+    #   room are already connected.
+    def connectRoom(self, room):
         if self.isConnectedTo(room):
             raise ValueError("These two rooms are already connected.")
         else:
-            self.connectedRooms.append(room)
-            room.connectedRooms.append(self)  # This goes both ways
+            pass  # TODO
 
-    ## Remove connected room
-    # @errors Won't error if self and room aren't connected
-    def removeConnectedRoom(self, room):
+    ## Disconnects two rooms
+    # @param room
+    #   The room to disconnect from this room
+    def disconnectRoom(self, room):
+        pass  # TODO
 
-        # Check that connection goes both ways; otherwise error out
-        if self.isConnectedTo(room):
-            self.connected_rooms.remove(room)
-            room.connected_rooms.remove(self)
-
-    ##  Check if this room is connected to another
-    # @throws ValueError Thrown if self and room are only connected in one way (indicates an error somewhere upstream)
+    ## Reports whether two rooms are connected
+    # @param room
+    #   The room which is check whether it is connected to
+    #   this room.
+    # @return
+    #   Returns whether this room is connected to the passed in room.
     def isConnectedToRoom(self, room):
-        counter = 0
-        if room in self.connectedRooms:
-            counter += 1
-        if self in room.connectedRooms:
-            counter += 1
+        pass  # TODO
 
-        # Error check
-        if counter == 1:
-            raise ValueError("The connection between these rooms only goes one way (i.e. A --> B, instead of A <-> B).")
 
-        # Done!
-        return counter == 2
+class TestRoom(TestCase):
+    def setup(self):
+        self.room = Room("testRoom")
 
-    # What is the point of this? furniture and self.furniture seem like the same thing - Ace
-    # (were you intending furniture to be something outside of this class? if so, you should
-    #  pass it in as an argument)
-    def spawnObjects(self):
-        """
-        Spawns all objects that are currently in the furniture dictionary
-        """
-        for obj in self.furniture:
-            self.placeFurniture(self.furniture[obj], obj[0], obj[1])
+    def testInit(self):
+        self.assertEqual(self.room.name, "testRoom")
+        self.assertTrue(self.room.getConnectedRooms().empty())
 
-    def removeFurniture(self, furn):
-        """
-        Delete all pieces of furniture in the furniture dictionary with the name {furn}.
-        """
-        for coords in self.furniture:
-            if self.furniture[coords] == furn:
-                del self.furniture[coords]
-        pass
+    def testConnectAValidRoom(self):
+        roomTwo = Room("testRoom2")
+        self.room.connectRoom(roomTwo)
+        self.assertListEqual(self.room.getConnectedRooms(), [roomTwo.name])
+
+    def testConnectAnInvalidRoom(self):
+        with self.assertRaises(TypeError):
+            self.room.connectedRoom("NotARoom")
+
+    def testConnectARoomAlreadyConnected(self):
+        roomTwo = Room("testRoom2")
+        self.room.connectRoom(roomTwo)
+        with self.assertRaises(ValueError):
+            self.room.connectedRoom(roomTwo)
+
+    def testDisconnectRoom(self):
+        roomTwo = Room("testRoom2")
+        self.room.connectedRoom(roomTwo)
+        self.room.disconnectRoom(roomTwo)
+        self.assertTrue(self.room.getConnectedRooms().empty())
+
+    def testDisconnectNotARoom(self):
+        with self.assertRaises(TypeError):
+            self.room.disconnectRoom("NotARoom")
+
+    def testDisconnectNotConnectedRoom(self):
+        roomTwo = Room("testRoom2")
+        with self.assertRaises(KeyError):
+            self.room.disconnectRoom(roomTwo)
+
+    def testIsConnectedToActuallyConnected(self):
+        roomTwo = Room("testRoom2")
+        self.room.connectRoom(roomTwo)
+        self.assertTrue(self.room.isConnectedTo(roomTwo))
+
+    def testIsConnectedNotConnected(self):
+        roomTwo = Room("testRoom2")
+        self.assertFalse(self.room.isConnectedTo(roomTwo))
+
+    def testIsConnectedNotARoom(self):
+        with self.assertRaises(TypeError):
+            self.room.isConnectedTo("NotARoom")
 
 if __name__ == "__main__":
-    room1104 = Room({(0,0):"couch", (2,2):"chair"})
-    print room1104.furniture
-
-    room2405 = Room({(2,4):"snack table"})
-    print room2405.furniture
-
-
+    main()
