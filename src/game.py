4f11a44a363d7fcd2845f553a21882c99deb7663
Fabian Junge <fabsjunge@gmail.com>
2014-04-25 17:46:52 -0500

    re-writting comments to be more informative

--- src/game.py
+++ src/game.py
@@ -1,46 +1,56 @@
 from objects.team import Team
-from objects.room import Room
 import map_functions
 import action_handler
 
-STARTING_ROOM = (72, 0, 255, 255)
+STARTING_ROOM = (72, 0, 255, 255)  # For testing purposes only
 
 
+## This is the class which the server sees as representing the entire game
 class Game(object):
+    # Objects:
+    # rooms: A list of all of the rooms
+    # turn: Which turn it is
+    # turn_limit: The maximum length the game will run
+    # action_buffer: A list of actions to be performed at the next 'tick'
+    # msg_buffer: A dictionary, indexed by client, of lists of responses to actions
+    # teams: A list of all of the teams
+    # people: A list of all of the people
 
-    # Initialize the server (only called once)
-
-    def __init__(self, file_url):
-
-        # the map reader will return a list of rooms that have bee
-        # linked together as defined in the design doc.
-        self.rooms = {i.name: i for i in map_functions.map_reader(file_url)}
+    ## Called by the server to have the game set itself up
+    # @param map_file
+    #   the file in which the map is located
+    def __init__(self, map_file):
+        self.rooms = {i.name: i for i in map_functions.map_reader(map_file)}
         self.turn = 0
-        #self.turn_limit = retrieveConstants("generalInfo")["TURNLIMIT"]
-        self.turn_limit = 80
+        # self.turn_limit = retrieveConstants("generalInfo")["TURNLIMIT"]
+        self.turn_limit = 80  # For testing purposes only
         self.action_buffer = []
         self.msg_buffer = {}
         self.teams = {}
         self.people = []
 
     ##  Adds a new team and returns success / failure message
-    #   @param data The data sent by the player to set up state
-    #   @param client_id The ID assigned to that player by the server
-    #   @return A (bool, dict) tuple stating success or failure and listing errors or sending starting info to the player
+    # @param data
+    #   The data sent by the player to set up state
+    # @param client_id
+    #   The ID assigned to that player by the server
+    # @return
+    #   A (bool, dict) tuple stating success or failure and listing errors or sending starting info to the player
     def add_new_team(self, data, client_id):
         response = {"status": "Success", "errors": []}
         try:
             newTeam = Team(data["team"], data["members"],
-                       self.rooms[STARTING_ROOM], self.people)
+                           self.rooms[STARTING_ROOM], self.people)
         except KeyError:
-            return (False, {"status": "Failure", "errors": ["KeyError"]}) #TODO: Make all error objects uniform
+            return (False, {"status": "Failure", "errors": ["KeyError"]})  # TODO: Make all error objects uniform
         self.msg_buffer[client_id] = []
         self.teams[client_id] = newTeam
 
         return (True, response)
 
-    ##  Actually execute queued actions
-    #   @return True if the game is running, False if the game ended
+    ## Execute queued actions
+    # @return
+    #   True if the game is running, False if the game ended
     def execute_turn(self):
         action_handler.handleTurn(self, self.action_buffer)
         self.action_buffer = []
@@ -49,10 +59,13 @@ class Game(object):
             return False
         return True
 
-    ##  Get these actions ready to execute
-    #   @param action_list A list of actions to be queued
-    #   @param client_id The ID of the player sending those actions
-    #   @return A list of errors for invalid actions
+    ## Queues all of the actions one client is attempting to execute this turn
+    # @param action_list
+    #   A list of actions the client wishes to perform
+    # @param client_id
+    #   The ID of the client sending these actions
+    # @return
+    #   A list of errors for invalid actions
     def queue_turn(self, action_list, client_id):
         error_list = []
         for action in action_list:
@@ -64,10 +77,12 @@ class Game(object):
                                    "action": action["action"]})
         return error_list
 
-    ##  Given client_id, returns the data to be sent back to the player.
-    #   If the game is over, send end-of-game stuff
-    #   @param client_id the identifier for the player to give info to
-    #   @return A dictionary containing the info to be sent to the player
+    ## Provides the information to be sent to a client each turn
+    #  If the game is over, send end-of-game stuff
+    # @param client_id
+    #   the client to which the information will be provided
+    # @return
+    #   A dictionary containing the info to be sent to the player
     def get_info(self, client_id):
         #TODO: Check for end of game, then do scoring and return the winner
         response = {"warnings": [],
@@ -75,4 +90,3 @@ class Game(object):
                     "messages": self.msg_buffer[client_id]}
         self.msg_buffer[client_id] = []
         return response
-        
