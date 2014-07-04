fcbb80cd97b185b14ea7c42ed5f7e1c45bafbcce
acen-uiuc <ace.nassri@gmail.com>
2014-07-04 15:38:00 -0700

    Got the visualizer running

--- src/vis/visualizer.py
+++ src/vis/visualizer.py
@@ -2,6 +2,7 @@ import pygame
 import config.handle_constants
 import json
 import random
+import time
 
 NO_CHAIR = (-100, -100)
 
@@ -17,16 +18,20 @@ class Visualizer( object ):
         self.colors = self.constants["TEAMCOLORS"]
         for i in range(len(self.colors)):
             self.colors[i] = tuple(self.colors[i])
-        self.rooms = rooms
-        # suffule seat assinment
-        for r in self.rooms.values():
-            random.shuffle(r.chairs)
         self.people = list()
         self.ai = list()
         self.team_names = list()
         self.messages = list()
         self.game_done = False
         self.game_result = None
+        self.rooms = rooms
+        self.quitWhenDone = self.constants['QUIT_WHEN_DONE']
+        
+        # shuffle seat assignment
+        if self.rooms:
+            for r in self.rooms.values():
+                random.shuffle(r.chairs)
+
         pygame.init()
         self.setup()
 
@@ -38,13 +43,32 @@ class Visualizer( object ):
         self.background = pygame.transform.scale(image,(self.SCREEN_WIDTH - self.constants["STATSBARWIDTH"], self.SCREEN_HEIGHT))
 
     def run_from_file(self, file_name=""):
-        while(self.running):
-            self.frame()
+
+        # Load game log
+        json_file = None
+        try:
+    		json_file = open(file_name)
+        except:
+            self.running = False
+            print "ERROR: Invalid game logfile."
+            return
+
+        # Run the game
+        i = 0
+        for turn_str in json_file:
+            json_turn = json.loads(turn_str)
+            self.frame(json_turn)
+
+        # If game is done and we're supposed to quit on exit, wait a while then exit
+        if self.quitWhenDone:
+            print "Game done; exiting"
+            time.sleep(5)
+            pygame.quit()
 
     def frame(self, turn=None):
         if self.running:
-            d = self.update_state(json.loads(turn))
-            while True and d:
+            d = self.update_state(turn)
+            while d:
                 self.draw()
                 self.GameClock.tick(self.MAX_FPS)
                 for event in pygame.event.get():
@@ -86,23 +110,23 @@ class Visualizer( object ):
 
         #Draw actions (move animations? failure prompts?)
 
-        #If game is over, do stuff
+        #If game is over, show winner
         if self.game_done:
             for i in range(len(self.game_result)):
                 if self.game_result[i]["winner"]:
                     gameoverfont = pygame.font.SysFont("monospace", 100)
-                    label = gameoverfont.render(self.team_names[i]+ " WINS!", 40, (12, 12, 12))
+                    label = gameoverfont.render(self.team_names[i]+ " WINS!", 35, (12, 12, 12))
                     self.ScreenSurface.blit(label, (0, 0))
 
         #flip display
         pygame.display.flip()
 
     def update_state(self, turn):
-        # check to see if the game as ended 
+        # check to see if the game has ended 
         if "winner" in turn[0]:
             self.game_done = True
             self.game_result = turn
-            return True
+            return not self.quitWhenDone
         if "team_name" in turn[0]:
             self.add_teams(turn)
             return False
@@ -168,5 +192,5 @@ class VisPerson(object):
 
 if __name__ == "__main__":
     vis = Visualizer()
-    vis.run_from_file()
+    vis.run_from_file("../serverlog.json")
 
