074e196ba047b7efcd635e3f3ec0e20d676be339
Nick Jeffrey <nickljeffrey@gmail.com>
2014-09-12 23:14:31 -0500

    removed no longer needed tests in action handler

--- src/action_handler.py
+++ src/action_handler.py
@@ -58,11 +58,6 @@ class TestActionHandler(unittest.TestCase):
     def setUp(self):
         pass
 
-    ## Test that the response() function formats the response correctly.
-    @unittest.skip("Not yet implemented")
-    def testResponse(self):
-        self.assertTrue(False)
-
     ## Test that the sortActions() function correctly sorts the actions.
     @unittest.skip("Needs updating")
     def testSortAction(self):
@@ -88,18 +83,6 @@ class TestActionHandler(unittest.TestCase):
         bufferAction(pseudoBuffer, pseudoAction, {"target": 'pseudoTarget', "person_id": 12}, 0)
         self.assertFalse(not pseudoBuffer)
         self.assertEquals(pseudoBuffer[0].action, validActions[0])
-
-    ## Tests that the executeAction() function correctly executes
-    #  the action given a valid action.
-    @unittest.skip("Not yet implemented")
-    def testExecuteActionExists(self):
-        self.assertTrue(False)
-
-    ## Tests that the executeAction() function correctly
-    #  responds given an invalid action.
-    @unittest.skip("Not yet implemented")
-    def testExecuteActionNotExists(self):
-        self.assertTrue(False)
 
     ## Test that handleTurn returns the correct list of responses.
     @unittest.skip("Not yet implemented")
