e3a75f6c6b40cfd0b1e33a650122010c981cb8b1
Jonathan Kirby <jrkirby2@illinois.edu>
2014-09-19 22:51:14 -0500

    animation rotations

--- src/vis/animation.py
+++ src/vis/animation.py
@@ -24,18 +24,32 @@ class Animation(object):
         pygame.PixelArray(teamImage).replace(pygame.Color(255, 0, 255, 255), color)
         return teamImage
 
+    def rot_center(self, image, angle):
+        """rotate an image while keeping its center and size"""
+        orig_rect = image.get_rect()
+        rot_image = pygame.transform.rotate(image, angle)
+        rot_rect = orig_rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        rot_image = rot_image.subsurface(rot_rect).copy()
+        return rot_image
+
     def draw(self, person, frame):
         scale_pos = self.scale((person.pos[0], person.pos[1]))
         if person.action == "move":
-            self.ScreenSurface.blit(self.animations["WALK"][(frame / 4) % 4], [p - 16 for p in scale_pos])
+            rot_image = self.rot_center(self.animations["WALK"][(frame / 4) % 4], person.rotation)
+            self.ScreenSurface.blit(rot_image, [p - 16 for p in scale_pos])
         elif person.action == "spy":
-            self.ScreenSurface.blit(self.animations["SPY"][(frame / 4) % 5], [p - 16 for p in scale_pos])
+            rot_image = self.rot_center(self.animations["SPY"][(frame / 4) % 5], person.rotation)
+            self.ScreenSurface.blit(rot_image, [p - 16 for p in scale_pos])
         elif person.action == "eat":
-            self.ScreenSurface.blit(self.animations["SPY"][(frame / 4) % 3], [p - 16 for p in scale_pos])
+            rot_image = self.rot_center(self.animations["EAT"][(frame / 4) % 3], person.rotation)
+            self.ScreenSurface.blit(rot_image, [p - 16 for p in scale_pos])
         elif person.action == "sleep":
-            self.ScreenSurface.blit(self.animations["SLEEP"][0], [p - 16 for p in scale_pos])
+            rot_image = self.rot_center(self.animations["SLEEP"][0], person.rotation)
+            self.ScreenSurface.blit(rot_image, [p - 16 for p in scale_pos])
         else:
-            self.ScreenSurface.blit(self.animations["STAND"][0], [p - 16 for p in scale_pos])
+            rot_image = self.rot_center(self.animations["STAND"][0], person.rotation)
+            self.ScreenSurface.blit(rot_image, [p - 16 for p in scale_pos])
         if person.action == "code":
             self.ScreenSurface.blit(self.animations["CODE"][0], [scale_pos[0] - 8, scale_pos[1] - 48])
             
