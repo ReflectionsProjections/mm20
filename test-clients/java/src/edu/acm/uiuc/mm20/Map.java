521f12bbd95db5e8d57fd54a6158847bc3c60c4d
Sam Laane <silverdev@ymail.com>
2014-09-22 20:57:39 -0500

    generated java objects and fixed issues

---
+++ test-clients/java/src/edu/acm/uiuc/mm20/Map.java
@@ -0,0 +1,46 @@
+
+package edu.acm.uiuc.mm20;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@Generated("org.jsonschema2pojo")
+public class Map {
+
+    @SerializedName("7")
+    @Expose
+    private edu.acm.uiuc.mm20._7 _7;
+
+    public edu.acm.uiuc.mm20._7 get7() {
+        return _7;
+    }
+
+    public void set7(edu.acm.uiuc.mm20._7 _7) {
+        this._7 = _7;
+    }
+
+    public Map with7(edu.acm.uiuc.mm20._7 _7) {
+        this._7 = _7;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+}
---
+++ test-clients/java/src/edu/acm/uiuc/mm20/_5.java
@@ -0,0 +1,76 @@
+
+package edu.acm.uiuc.mm20;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@Generated("org.jsonschema2pojo")
+public class _5 {
+
+    @SerializedName("person_id")
+    @Expose
+    private Long personId;
+    @Expose
+    private String name;
+    @Expose
+    private Long team;
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public _5 withPersonId(Long personId) {
+        this.personId = personId;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public _5 withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Long getTeam() {
+        return team;
+    }
+
+    public void setTeam(Long team) {
+        this.team = team;
+    }
+
+    public _5 withTeam(Long team) {
+        this.team = team;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+}
