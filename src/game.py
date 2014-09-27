b1af2d8e3d78e1253342f49736fa791f4c2de8d7
Nick Jeffrey <nickljeffrey@gmail.com>
2014-09-27 17:16:14 -0500

    removed snack table depletion

--- src/game.py
+++ src/game.py
@@ -41,7 +41,6 @@ class Game(object):
         self.professorroom = ""
         self.professortime = 0
         self.professorhours = defaults["PROFESSORHOURS"]
-        self.snackrefill = defaults["SNACKREFILL"]
 
     ##  Adds a new team and returns success / failure message
     # @param data
@@ -120,12 +119,6 @@ class Game(object):
             self.professortime = self.professorhours * self.turn_limit / 24
             self.rooms[self.professorroom].addResource("PROFESSOR")
             self.event_notification("PROFESSOR", self.professorroom)
-        for r in self.rooms.values():
-            if self.turn % (self.turn_limit / 24) == 0:
-                if "FOOD" in r.resources:
-                    r.snacksupply = self.snackrefill
-            if "FOOD" in r.resources and r.snacksupply < 1:
-                self.event_notification("DEPLETEDSNACKTABLE", r.name)
         self.turn += 1
         if self.turn >= self.turn_limit:
             return False
@@ -245,14 +238,6 @@ class TestGame(unittest.TestCase):
 
     @unittest.skip("Not yet implemented")
     def testPractice(self):
-        self.assertTrue(False)
-
-    @unittest.skip("Not yet implemented")
-    def testSnackdeplete(self):
-        self.assertTrue(False)
-
-    @unittest.skip("Not yet implemented")
-    def testSnackrefill(self):
         self.assertTrue(False)
 
     @unittest.skip("Not yet implemented")
